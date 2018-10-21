56e5aa955a86ae0f39829771c820e1560853b554
VernomPham <ngoctupham1997@gmail.com>
2018-10-21 12:42:23 +0700

    Sửa lỗi addchild->nullptr Signed-off-by: VernomPham <ngoctupham1997@gmail.com>

--- Classes/GamePlayLayer.cpp
+++ Classes/GamePlayLayer.cpp
@@ -105,38 +105,44 @@ bool GamePlayLayer::init()
 	_pauseBtn->addTouchEventListener(CC_CALLBACK_2(GamePlayLayer::TouchPauseButton, this));
 	this->addChild(_pauseBtn);
 	// all button
-	_resumeBtn = cocos2d::ui::Button::create("images/PlayButton4.png");
+	_resumeBtn = cocos2d::ui::Button::create("images/PauseButton.png");
 	_resumeBtn->setPosition(Vec2(visibleSize.width*0.5f, visibleSize.height*0.7f));
 	_resumeBtn->setScale(1.0f);
 	_resumeBtn->setTag(1);
+	_resumeBtn->setVisible(false);
+
 	_resumeBtn->addTouchEventListener(CC_CALLBACK_2(GamePlayLayer::TouchResumeButton, this));
-	this->addChild(_resumeBtn, 2);
+	this->addChild(_resumeBtn);
 	//Label Play
-	auto _labelPlay = Label::createWithTTF("Resume", "fonts/kenvector_future.ttf", 25);
-	_labelPlay->setPosition(_resumeBtn->getPosition());
-	this->addChild(_labelPlay, 2);
-	_shopBtn = cocos2d::ui::Button::create("images/PlayButton4.png");
+	_labelResume = Label::createWithTTF("Resume", "fonts/kenvector_future.ttf", 25);
+	_labelResume->setPosition(_resumeBtn->getPosition());
+	_labelResume->setVisible(false);
+	this->addChild(_labelResume);
+	_shopBtn = cocos2d::ui::Button::create("images/PauseButton.png");
 	_shopBtn->setPosition(Vec2(visibleSize.width*0.5f, visibleSize.height*0.5f));
 	_shopBtn->setScale(1.0f);
 	_shopBtn->setTag(1);
+	_shopBtn->setVisible(false);
 	_shopBtn->addTouchEventListener(CC_CALLBACK_2(GamePlayLayer::TouchShopButton, this));
-	this->addChild(_resumeBtn, 2);
+	this->addChild(_shopBtn);
 	//Label Play
-	auto _labelShop = Label::createWithTTF("Shop", "fonts/kenvector_future.ttf", 25);
+	_labelShop = Label::createWithTTF("Shop", "fonts/kenvector_future.ttf", 25);
 	_labelShop->setPosition(_shopBtn->getPosition());
-	this->addChild(_labelShop, 2);
+	_labelShop->setVisible(false);
+	this->addChild(_labelShop);
 	//Button Quit
-	_quitBtn = cocos2d::ui::Button::create("images/QuitButton3.png");
+	_quitBtn = cocos2d::ui::Button::create("images/PauseButton.png");
 	_quitBtn->setPosition(Vec2(visibleSize.width*0.5f, visibleSize.height*0.3f));
 	_quitBtn->setScale(1.0f);
-	_quitBtn->setTag(2);
+	_quitBtn->setTag(1);
+	_quitBtn->setVisible(false);
 	_quitBtn->addTouchEventListener(CC_CALLBACK_2(GamePlayLayer::TouchQuitButton, this));
-	this->addChild(_quitBtn, 2);
+	this->addChild(_quitBtn);
 	//Label Quit
-	auto _labelQuit = Label::createWithTTF("Quit", "fonts/kenvector_future.ttf", 25);
+	_labelQuit = Label::createWithTTF("Quit", "fonts/kenvector_future.ttf", 25);
 	_labelQuit->setPosition(_quitBtn->getPosition());
-	this->addChild(_labelQuit, 2);
-
+	_labelQuit->setVisible(false);
+	this->addChild(_labelQuit);
 	return true;
 }
 void GamePlayLayer::IconCoinCreate()
@@ -163,24 +169,25 @@ void GamePlayLayer::TouchPauseButton(Ref* pSender, cocos2d::ui::Widget::TouchEve
 	if (eEventType == cocos2d::ui::Widget::TouchEventType::ENDED)
 	{
 		Director::getInstance()->pause();
-		_resumeBtn->setVisible(false);
-		_quitBtn->setVisible(false);
-		_shopBtn->setVisible(false);
+		_resumeBtn->setVisible(true);
+		_labelResume->setVisible(true);
+		_quitBtn->setVisible(true);
+		_labelQuit->setVisible(true);
+		_shopBtn->setVisible(true);
+		_labelShop->setVisible(true);
 	}
 }
 void GamePlayLayer::TouchResumeButton(Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType)
 {
 	if (eEventType == cocos2d::ui::Widget::TouchEventType::ENDED)
 	{
-		auto a = this->getChildByTag(1);
 		Director::getInstance()->resume();
-		if (a)
-		{
-			_resumeBtn->setVisible(true);
-			_quitBtn->setVisible(true);
-			_shopBtn->setVisible(true);
-		}
-
+		_resumeBtn->setVisible(false);
+		_labelResume->setVisible(false);
+		_quitBtn->setVisible(false);
+		_labelQuit->setVisible(false);
+		_shopBtn->setVisible(false);
+		_labelShop->setVisible(false);
 	}
 }
 void GamePlayLayer::TouchShopButton(Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType)
