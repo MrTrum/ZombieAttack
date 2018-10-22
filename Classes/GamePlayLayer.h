205812f49a5b1ccef838ac195bfc1aa5bdb3113c
Nido <kaelzee@gmail.com>
2018-10-22 22:05:34 +0700

    fix bug

--- Classes/GamePlayLayer.h
+++ Classes/GamePlayLayer.h
@@ -5,7 +5,6 @@
 #include "ui/CocosGUI.h"
 #include "ui/UIButton.h"
 #include "Coin.h"
-#include "Money.h"
 
 
 class BackgroundLayer;
@@ -22,20 +21,13 @@ public:
 	CREATE_FUNC(GamePlayLayer);
 	virtual bool init();
 	static cocos2d::Scene* createGamePlayLayer();
-	void IconCoinCreate();
-	void TouchPauseButton(Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType);
-	void TouchResumeButton(Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType);
-	void TouchShopButton(Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType);
-	void TouchQuitButton(Ref* pSender, cocos2d::ui::Widget::TouchEventType eEventType);
-	void update(float dt);
-	void removeCoin();
-	void test();
+
 protected:
 	bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
 	void onTouchMoved(Touch* touch, Event* event);
 	void onTouchEnded(Touch* touches, Event* event);
 	void Shooting(Touch *touch);
-	bool onContactBegan(cocos2d::PhysicsContact& contact);
+
 private:
 	Size			winSize;
 	BackgroundLayer *_bg;
@@ -45,17 +37,10 @@ private:
 	float			_posX;
 	float			_posY;
 	Sprite*			_Barrier;
+	Coin*			_Coin;
 	Sprite*			_hpbar;
-	Coin* _Coin;
-	Money* _Money;
-	int _totalMoney = 0;
-	bool _checkMoney = false;
-	cocos2d::Label* _labelResume;
-	cocos2d::Label*_labelShop;
-	cocos2d::Label*_labelQuit;
-	cocos2d::ui::Button* _resumeBtn;
-	cocos2d::ui::Button* _quitBtn;
-	cocos2d::ui::Button* _shopBtn;
+
+};
 
 #endif // !_GAME_PLAY_LAYER
 
